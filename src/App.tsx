5be414691c00542dfbd79cb90f0243cc3701b405
Daffario Alexandra <daffarioalexandra@gmail.com>
2022-08-25 01:17:40 +0700

    membuat route yang tidak tersedia

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import LayoutComponent from "./components/layout";
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
+import NotFound from "./pages/NotFound";
 import Profile from "./pages/Profile";
 
 export default function App() {
@@ -13,6 +14,8 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="profile" element={<Profile />} />
           <Route path="movies" element={<Movies />} />
+          <Route path="*" element={<NotFound />} />
+          
         </Routes>
       </LayoutComponent>
     </>
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+    return <h1>Maaf halaman yang anda kunjungi tidak tersedia!</h1>
+}
